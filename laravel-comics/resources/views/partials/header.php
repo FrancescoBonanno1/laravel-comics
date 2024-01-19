913eb88cfb632744f15789b05d4114bbd12f43f6
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-19 16:43:25 +0100

    problema con i percorsi

---
+++ laravel-comics/resources/views/partials/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="/laravel-comics/public/style.css">
+</head>
+<body>
+    <div id="small_lip">
+        <h3>DC POWER VISA</h3>
+        <h3>ADDITIONAL DC SITES</h3>
+    </div>
+    <header id="nav">
+        <div id="img_container">
+    <img src="laravel-comics/public/images/dc-logo.png" alt="logo dc">
+        </div>
+
+    </header>
+</body>
+</html>
