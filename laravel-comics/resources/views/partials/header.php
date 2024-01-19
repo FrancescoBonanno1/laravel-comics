21786c67dd34ba18d688a03227831fc62f295cc9
FrancescoBonanno1 <francescosoapbonanno@gmail.com>
2024-01-19 17:23:37 +0100

    primo schizzo di un componente header creato

--- laravel-comics/resources/views/partials/header.php
+++ laravel-comics/resources/views/partials/header.php
@@ -1,21 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel="stylesheet" href="/laravel-comics/public/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <div id="small_lip">
         <h3>DC POWER VISA</h3>
         <h3>ADDITIONAL DC SITES</h3>
     </div>
-    <header id="nav">
+    <header>
         <div id="img_container">
-    <img src="laravel-comics/public/images/dc-logo.png" alt="logo dc">
+            <img src="/images/dc-logo.png" alt="dc-logo">
+
         </div>
 
     </header>
 </body>
+
 </html>
